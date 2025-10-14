06112ff7e3f331d23e77d8869b420ce8912759a3
lzkostina <kostina@umich.edu>
2025-10-13 22:45:55 -0400

    run_experiment: description added

--- run_experiment.py
+++ run_experiment.py
@@ -5,10 +5,22 @@
 ## Signal-to-noise ratio: overall signal strength snrs
 #####################################################################
 
+"""
+    n : int
+        Number of observations.
+    df : float
+        Degrees of freedom for Student-t error distribution.
+    gamma : float
+        Aspect ratio p/n.
+    rho : float
+        AR(1) correlation parameter in [0, 1). Cov(X_j, X_k) = rho**|j-k|.
+    snr : float
+        Target signal-to-noise ratio defined as (beta^T X^T X beta) / sigma^2.
+"""
 PARAM_GRID = {
     "dfs": [1, 2, 3, 20, float("inf")],
     "gammas": [0.2, 0.5, 0.8],   # p/n
-    "rhos": [0.0, 0.25, 0.5, 0.75, 1.0],
+    "rhos": [0.0, 0.25, 0.5, 0.75],
     "snrs": [1, 5, 10],
     "reps": 30,
     "n": 500,                    # base n
